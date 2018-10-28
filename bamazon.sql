73a51f8c7cbe3378409947f6743790eb9f8a2c0f
Peter Florek <peter.florek@yahoo.com>
2018-10-27 21:09:04 -0400

    saving work

---
+++ bamazon.sql
@@ -0,0 +1,28 @@
+DROP DATABASE If EXISTS bamazon_db;
+
+CREATE DATABASE bamazon_db;
+
+USE bamazon_db;
+
+CREATE TABLE products (
+  item_id INT NOT NULL AUTO_INCREMENT,
+  product_name VARCHAR(100) NOT NULL,
+  department VARCHAR(50) NOT NULL,
+  price DECIMAL(10,2) NOT NULL,
+  stock_qty INT NOT NULL,
+  PRIMARY KEY (item_id)
+);
+
+INSERT INTO products
+(product_name, department, price, stock_qty)
+VALUES
+("Current Fish Tank 10gal", "Pet Supplies", 15.99, 20),
+("Prana XL Yoga Mat", "Sports & Exercise", 49.99, 8),
+("Home Bar Kit, 12pc.", "Housewares", 40.00, 3),
+("HP All-in-one Officejet Wireless Printer", "Computers & Electronics", 250.00, 10),
+("Fancy Jumbo Paperclips, 100ct.", "Office Supplies", 3.99, 967),
+("New Balance Impulse Men's Running Shoes", "Sports & Exercise", 159.99, 2),
+("Fender Telecaster - Beginner Electric Guitar w/Gig bag", "Musical Instruments", 199.99, 4),
+("TempurCloud Pillow by Tempur-Pedic", "Home", 149.00, 31),
+("Apple iPad 6th Gen 128GB", "Computers & Electronics", 469.00, 6),
+("Ibanez Mandolin - f-model two-tone sunburst", "Musical Instruments", 165.00, 1);
---
+++ bamazonCustomer.js
@@ -0,0 +1,214 @@
+// define dependencies
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+
+// connection criteria for db
+const db = mysql.createConnection({
+  host: "localhost",
+
+  port: 3306,
+
+  user: "root",
+
+  password: "",
+  database: "bamazon_db"
+})
+
+// connect to db
+db.connect((err) => {
+
+  if (err) throw err;
+
+  console.log(`connection established with id ${db.threadId}
+  
+  Welcome to Bamazon!`);
+
+  // db.end();
+  mainMenu();
+
+});
+
+
+// define functions
+// function for initial search options
+const mainMenu = () => {
+
+  inquirer.prompt([
+    {
+      name: "searchOption",
+      type: "list",
+      message: "Browse...",
+      choices: ["products by department", "products by item name", "all products"]
+    }
+
+  ])
+    .then(userResponse => {
+
+      switch (userResponse.searchOption) {
+        case "products by department":
+          deptSearch();
+          break;
+
+        case "products by item name":
+          itemSearch();
+          break;
+
+        case "all products":
+          allSearch();
+          break;
+
+      }
+    });
+}
+
+// function for searching by department
+const deptSearch = () => {
+
+  const query = db.query("SELECT department FROM products",
+    (err, prodData) => {
+      if (err) throw err;
+
+      inquirer.prompt([
+        {
+          name: "deptOption",
+          type: "list",
+          message: "Select below to see all products available in a particular department.",
+          choices: prodData.map(dept => dept.department).filter((department, index, self) => self.indexOf(department) === index)
+
+        }
+      ])
+        .then(choice => {
+
+          console.log(`*******NOW VIEWING ALL ${choice.deptOption} PRODUCTS*******`)
+
+          const query = db.query("SELECT * FROM products WHERE department = ?",
+            [choice.deptOption], (err, prodByDept) => {
+
+              if (err) throw err;
+
+              prodByDept.forEach((product) => {
+
+                console.log(`
+          Product: ${product.product_name}
+          Price: $${product.price}
+          Quantity Remaining: ${product.stock_qty}
+          Item #: ${product.item_id}
+        `)
+              });
+
+
+
+            });
+
+          selectItem();
+
+        });
+
+    });
+}
+
+// function to search for item by user input
+const itemSearch = () => {
+
+}
+
+const allSearch = () => {
+
+}
+
+// function for choosing a specific item by item #
+const selectItem = () => {
+
+  const query = db.query("SELECT item_id FROM products",
+    (err, prodData) => {
+      if (err) throw err;
+
+      // ask for item #
+      inquirer.prompt([
+        {
+          name: "itemChoice",
+          type: "input",
+          message: "Please enter the item # of the product you wish to purchase",
+          // validate: (itemInput) => {
+
+          //   const idArray = prodByDept.map(item => item.item_id);
+
+          //   console.log(`
+          //   Array: ${idArray}`)
+
+          //   if (idArray.includes(itemInput)) {
+
+          //     return true;
+
+          //   } else {
+
+          //     console.log(`
+          //     Sorry.
+
+          //     We couldn't locate that item.
+              
+          //     Please enter the item # exactly as it appears in the description.`);
+
+          //     return false;
+          //   }
+
+          // }
+        },
+        // ask for qty
+        {
+          name: "qtyChoice",
+          type: "input",
+          message: "How many would you like to purchase?",
+          default: 1,
+          validate: (qtyInput) => {
+            if (!isNaN(qtyInput)) {
+              return true;
+            } else {
+              console.log(`
+          Please enter a valid quantity.
+          `);
+              return false;
+            }
+
+          }
+        }
+      ]).then(user => {
+        // define if conditional to check if user qty is less than or equal to current product qty
+        if (user.qtyChoice <= prodData.stock_qty) {
+
+          console.log(`
+        You are about to purchase ${user.qtyChoice} of the following item:`);
+
+        const query = db.query("SELECT * FROM products WHERE item_id = ?",
+          [user.itemChoice], (err, prodById) => {
+
+            if (err) throw err;
+
+            prodById.forEach((product) => {
+
+              console.log(`Product: ${product.product_name}
+      @ $${product.price} each.
+    `)
+            });
+
+            // confirmSale();
+
+          });
+
+        } else {
+
+          console.log(`
+          There is insufficient quantity in stock. Please check available quantities and try again`)
+        }
+
+
+      })
+
+      // Close db.query here
+    });
+}
+
+// function to confirm sale/payment method/shipment method of item
+// const confirmSale = () => {
+
+// }
